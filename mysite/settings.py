66815dbd1a7bd7e4e4aa44d883f352ca15e91fcc
sophycolbert <sophycolbert@hotmail.com>
2025-09-20 15:44:49 +0100

    added gitignore file

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$hk^%(y*e_wydi^wx(^*%xks8%h-qh$70to=4tk17=1^zjju+u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com', 'sophycolbert.pythonanywhere.com']
 
 
 # Application definition
